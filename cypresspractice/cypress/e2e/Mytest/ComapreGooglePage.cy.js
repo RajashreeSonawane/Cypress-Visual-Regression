78fb9f511345e72b8dd3910549732061f51a87a1
Rajashree Sonawane <rajashrees@synerzipune.local>
2023-02-01 15:46:41 +0530

    remove gitignore files

--- cypresspractice/cypress/e2e/Mytest/ComapreGooglePage.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Visuals Regression Testing', () => {
-  it('should compare screenshot of the entire page', () => {
-    cy.visit('www.google.com')
-    cy.compareSnapshot('home-page', 0.2)
-  })
-})
